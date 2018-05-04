93264bb6eea1ba89a52a12fea49c4252772a61ab
tea kupradze <teakoo555@gmail.com>
2018-05-04 19:43:32 +0400

    add gesture

--- SocialNetwork/Controller/DetailScreen.swift
+++ SocialNetwork/Controller/DetailScreen.swift
@@ -10,7 +10,8 @@ import UIKit
 class DetailScreen: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    
+    @IBOutlet weak var contentView: UIView!
+    fileprivate var dataSource = [UserObject]()
     var dataObject: UserObject?
     
     override func viewDidLoad() {
@@ -23,6 +24,19 @@ class DetailScreen: UIViewController {
 
     }
 
+    func loadFirstData(){
+        for i in 0..<2 {
+            let object = UserObject()
+            object.profileImage = UIImage(named: "test_\(Int(arc4random_uniform(2)))")
+            self.dataSource.append(object)
+        }
+    }
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        loadFirstData()
+        
+     
+        
+    }
     @IBAction func backButtonClickd(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)    }
     
